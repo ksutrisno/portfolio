40d3cd6ba3ba380ed79559610ba9ee33805b151c
asubuntung9 <kevinjsutrisno@gmail.com>
2019-08-17 01:27:45 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import NavigationBar from './Navbar/NavigationBar';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import {BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom"
 
+import Home from './Pages/Home'
 
 class App extends Component{
     render()
     {
-      return(  
-          <NavigationBar/>
+      return(         
+          <Router>
+              <NavigationBar/>
+              <Route path = "/" component = {Home}/>
+          </Router>
       );
     }
 }
