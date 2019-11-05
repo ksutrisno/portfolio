9d8edef1556b125446b33ae9702a5fae8ab3faa5
ksutrisno <kevinjsutrisno@gmail.com>
2019-11-05 18:29:35 +0700

    commit

--- src/Navbar/Sidebar.jsx
+++ src/Navbar/Sidebar.jsx
@@ -7,10 +7,10 @@ function Sidebar() {
     return (
         <div>
         <div class = "social-links">
-            <a href ="#"  onClick={()=>window.open("https://linkedin.com/in/kevin-sutrisno" )}><i className = "fa fa-linkedin-square" aria-hidden = "true"></i></a>
-            <a href ="#"  onClick={()=>window.open("https://github.com/ksutrisno" )} ><i className = "fa fa-github-square" aria-hidden = "true"></i></a>
-            <a href ="#" onClick={()=>window.open("https://twitter.com/sutrisno_kevin" )}><i className = "fa fa-twitter-square" aria-hidden = "true"></i></a>
-            <a href ="#" onClick={()=>window.open("https://www.instagram.com/kejosus" )}><i className = "fa fa-instagram" aria-hidden = "true"></i></a>
+            <a href ="/#"  onClick={()=>window.open("https://linkedin.com/in/kevin-sutrisno" )}><i className = "fa fa-linkedin-square" aria-hidden = "true"></i></a>
+            <a href ="/#"  onClick={()=>window.open("https://github.com/ksutrisno" )} ><i className = "fa fa-github-square" aria-hidden = "true"></i></a>
+            <a href ="/#" onClick={()=>window.open("https://twitter.com/sutrisno_kevin" )}><i className = "fa fa-twitter-square" aria-hidden = "true"></i></a>
+            <a href ="/#" onClick={()=>window.open("https://www.instagram.com/kejosus" )}><i className = "fa fa-instagram" aria-hidden = "true"></i></a>
 
         </div>
 
