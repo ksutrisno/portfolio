0b04a206e532def8a90175847631fe76ad76d265
ksutrisno <kevinjsutrisno@gmail.com>
2022-01-26 14:28:35 +0700

    update

--- src/Navbar/Sidebar.jsx
+++ src/Navbar/Sidebar.jsx
@@ -11,8 +11,8 @@ function Sidebar() {
             <a href ="/#"  onClick={()=>window.open("https://github.com/ksutrisno" )} ><i className = "fa fa-github-square" aria-hidden = "true"></i></a>
             <a href ="/#" onClick={()=>window.open("https://twitter.com/sutrisno_kevin" )}><i className = "fa fa-twitter-square" aria-hidden = "true"></i></a>
             <a href ="/#" onClick={()=>window.open("https://www.instagram.com/kejosus" )}><i className = "fa fa-instagram" aria-hidden = "true"></i></a>
-
-        </div>
+            <a href ="/#" onClick={()=>window.open("https://www.youtube.com/channel/UCaYkQ1zUpXlqy4_lQkMVNJw")}><i className = "fa fa-youtube-square" aria-hidden = "true"></i></a>
+        </div>instagram
 
         <div class = "email">
              <e>kevinjsutrisno@gmail.com</e>
